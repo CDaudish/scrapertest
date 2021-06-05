49574cd99f5f901f6a0004bdeba9efb0f4253ca0
CDaudish <67439102+CDaudish@users.noreply.github.com>
2021-06-04 22:45:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,39 @@ import wikipedia
 import requests
 import random
 from flask_cors import CORS, cross_origin
+import json
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERs'] = 'Content-Type'
+
+def get_picture(wiki):
+    url = "http://flip1.engr.oregonstate.edu:4075/?" + wiki
+    r = requests.get(url).json()
+    r = r[1:]
+    r = r[:-1]
+    r = json.loads(r)
+    return r['url']
+
+def scrape_multiple_keyword(wiki):
+    summary = wikipedia.summary(wiki)
+    count = 0
+    res = ""
+    for ele in summary:
+        if ele == ' ':
+            count = count + 1
+            if res[-1] != ' ':
+                res = res + " "
+                
+        else:
+            if count % 6 == 0:
+                res = res + ele
+
+        if len(res) > 150:
+            break
+        
+    res_list = res.split()
+    return res_list
 
 def scrape_single_keyword(wiki_page):
     summary = wikipedia.summary(wiki_page)
@@ -37,25 +66,11 @@ def search():
 def search_result():
     if request.form['submit_button'] == 'Scrape':
         text = request.form['text']
-        summary = wikipedia.summary(text)
-        count = 0
-        res = ""
-        for ele in summary:
-            if ele == ' ':
-                count = count + 1
-                if res[-1] != ' ':
-                    res = res + " "
-                
-            else:
-                if count % 6 == 0:
-                    res = res + ele
 
-            if len(res) > 150:
-                break
-        
-        res_list = res.split()
+        res_list = scrape_multiple_keyword(text)
+        picture = get_picture(text)
 
-        return render_template("results.html", res_list = res_list)
+        return render_template("results.html", res_list = res_list, keywordimage = picture)
     elif request.form['submit_button'] == 'Encrypt':
         url = 'https://quiet-wave-78817.herokuapp.com/encrypt'
         password = "yourpassword"
